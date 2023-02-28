5c417262ddf0135e2903ce6c38570d847a4fc939
MaggieWesolowska <maggiewes07@gmail.com>
2023-02-28 15:15:30 +0100

    edited task-1 js and updated json

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,28 @@
-// Add imports above this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 import { galleryItems } from './gallery-items';
-// Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+
+const markup = galleryItems
+    .map(galleryItem => 
+        `<a class="gallery__item" href="${galleryItem.original}">
+             <img class="gallery__image" 
+                src="${galleryItem.preview}"
+                alt="${galleryItem.description}"/>
+        </a>`)
+    .join("");
+
+console.log(markup);
+
+gallery.insertAdjacentHTML("afterbegin", markup);
+
+let lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captions: true,
+    captionPosition: 'bottom',
+    captionDelay: 250,
+});
