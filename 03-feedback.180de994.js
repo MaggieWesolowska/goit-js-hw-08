f924dc693282c2e16ff7b9a2ca67a95f08c64c76
MaggieWesolowska <114258920+MaggieWesolowska@users.noreply.github.com>
2023-03-01 17:10:34 +0000

    Deploying to gh-pages from @ MaggieWesolowska/goit-js-hw-08@e9eebaca631faab0175af4ef6c5604822fcbac42 🚀

---
+++ 03-feedback.180de994.js
@@ -0,0 +1,2 @@
+document.querySelector("form"),document.querySelector("input"),document.querySelector("textarea"),document.querySelector("button");
+//# sourceMappingURL=03-feedback.180de994.js.map
---
+++ 03-feedback.b93052ce.js
@@ -0,0 +1,2 @@
+document.querySelector("form"),document.querySelector("input"),document.querySelector("textarea"),document.querySelector("button");
+//# sourceMappingURL=03-feedback.b93052ce.js.map
