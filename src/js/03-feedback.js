e9eebaca631faab0175af4ef6c5604822fcbac42
MaggieWesolowska <maggiewes07@gmail.com>
2023-03-01 18:10:06 +0100

    edited task-3 js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,6 @@
+const form = document.querySelector('form');
+const email = document.querySelector('input');
+const message = document.querySelector('textarea');
+const submit = document.querySelector('button');
+
 
