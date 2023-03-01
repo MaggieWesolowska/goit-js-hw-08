b79a5e3c0652644e1a8e874db0d95ff77b1722ce
MaggieWesolowska <maggiewes07@gmail.com>
2023-03-01 21:33:10 +0100

    edited comments

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,12 +1,7 @@
 import Player from '@vimeo/player';
-
 import throttle from 'lodash.throttle'
 
-// code example:
- // const throttled = throttle(renewToken, 1000);
-
 const iframe = document.querySelector('iframe');
-
 const player = new Player(iframe);
 
     player.on('play', function() {
@@ -21,8 +16,7 @@ const player = new Player(iframe);
         console.log(data);
         localStorage.setItem('videoplayer-current-time', data.seconds);
     };
-    
-    // data:
+    // data for event 'timeupdate':
     // {
     //     duration: 61.857
     //     percent: 1
@@ -30,10 +24,14 @@ const player = new Player(iframe);
     // }
 
     player.on('timeupdate', throttle(onPlay, 1000));
+    // examples:
+    // player.on('eventName', onPlay);
+    // player.on('eventName', function());
 
     const currentTime = Number(localStorage.getItem('videoplayer-current-time')) || 0;
 
     player.setCurrentTime(currentTime);
+    
 
 
 
