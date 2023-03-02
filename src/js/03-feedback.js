35f9039135ee1b68280b23925079015cd1edfbe4
MaggieWesolowska <maggiewes07@gmail.com>
2023-03-02 21:02:49 +0100

    edited task-3 js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,8 +1,35 @@
 // import '../css/03-feedback.css';
+import throttle from "lodash.throttle";
 
 const form = document.querySelector('form');
-const email = document.querySelector('input');
-const message = document.querySelector('textarea');
-const submit = document.querySelector('button');
+const email = document.querySelector('input').value;
+const message = document.querySelector('textarea').value;
+const submitBtn = document.querySelector('button');
+const LOCAL_STORAGE_KEY = 'feedback-form-state';
+
+const formInput = {
+    email,
+    message,
+}
+
+form.addEventListener('input', getFormInput);
+
+submitBtn.addEventListener('submit', () => {
+    console.log(formInput.value);
+    form.reset()
+})
+
+function getFormInput(event) {
+    event.preventDefault();
+    
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(formInput));
+    saveFormInput();
+    // throttle(storage, 500);
+}
+
+function saveFormInput() {
+    localStorage.getItem(LOCAL_STORAGE_KEY) || "";
+}
+
 
 
