988a13771246a284e1323b18b763cd001b3f3e22
MaggieWesolowska <maggiewes07@gmail.com>
2023-03-03 16:18:02 +0100

    edited task-3 js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,40 +1,34 @@
-// import '../css/03-feedback.css';
 import throttle from "lodash.throttle";
 
 const form = document.querySelector('form');
-const email = document.querySelector('input').value;
-const message = document.querySelector('textarea').value;
-const submitBtn = document.querySelector('button');
+const email = document.querySelector('input');
+const message = document.querySelector('textarea');
 const LOCAL_STORAGE_KEY = 'feedback-form-state';
 
-const formInput = {
-    email,
-    message,
-}
+form.addEventListener('input', throttle(getFormInput, 500));
 
-form.addEventListener('input', getFormInput);
-
-submitBtn.addEventListener('submit', () => {
-    // form.addEventListener('input', getFormInput);
-    console.log(formInput);
+form.addEventListener('submit', (event) => {
+    event.preventDefault()
+    const formElements = {
+        email: email.value,
+        message: message.value,
+    }
+    console.log(formElements);
+    localStorage.removeItem(LOCAL_STORAGE_KEY);
     form.reset()
 })
 
-function getFormInput(event) {
-    event.preventDefault();
-    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(formInput));
-    saveFormInput();
-    // throttle(storage, 500);
+function getFormInput() {
+    const formElements = {
+        email: email.value,
+        message: message.value,
+    }
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(formElements));
 }
 
-function saveFormInput() {
-    JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY) || "");
-    email = LOCAL_STORAGE_KEY.email.value;
-    message = LOCAL_STORAGE_KEY.message.value;
-}
-
-
-
-
-
-
+    let parsedInput = localStorage.getItem(LOCAL_STORAGE_KEY) ||'';
+         if (parsedInput !== '') {
+            parsedInput = JSON.parse(parsedInput);
+            email.value = parsedInput.email;
+            message.value = parsedInput.message;
+    }
